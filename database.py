7eb0f9b25f664d54a23dd95f5e724a93453e04c0
jx <jx@x2v3.com>
2016-12-16 14:00:52 +0800

    add query support

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import pymysql
-import logging
 
 class Database:
     mysql_server='localhost'
@@ -40,28 +39,43 @@ class Database:
             self.db_connection=pymysql.connect(host=self.mysql_server,user=self.mysql_user,password=self.mysql_pwd,db=self.mysql_db_name)
         return self.db_connection
 
+    def get_query_table(self,sql,*params):
+        conn=self.get_connection()
+        cursor=conn.cursor()
+        cursor.execute(sql,params)
+        cursor.close()
+        table=Datatable()
+
 
 class Datatable:
     name=''
     columns=None
     rows=None
     database=None
+    query_sql=None
 
-    def get_columns(self):
+    def __init__(self,name,query):
+        if name is not None:
+            self.name=name
+        if query is not None:
+            self.query_sql=query
+
+    def get_columns(self):        
         if self.columns is None:
-            sql='select * from %s'%self.name
             self.query_table_info(sql)
         return self.columns
 
     def get_rows(self):
         if self.rows is None:
-            sql='select * from %s'%self.name
             self.query_table_info(sql)
         return self.rows
 
-    def query_table_info(self,sql,*args):
+    def query_table_info(self):
+        sql='select * from `%s`'%self.name
+        if self.query_sql is not None:
+            sql=self.query_sql
         cursor=self.database.get_connection().cursor()
-        cursor.execute(sql,args)
+        cursor.execute(sql)
         rs=cursor.fetchall()
         self.columns=[c[0] for c in cursor.description]
         self.rows=rs
